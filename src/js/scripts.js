65ba0473c0da8445493ca121c3d156c25c710292
Ashley Kolodziej <kolodz10@bu.edu>
2020-10-30 18:06:53 -0400

    Roll back dependencies

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,11 @@
-/* eslint-disable */
-
-// $(document)(function(){
-//     console.log('test')
-//     function initMap() {
-//         // The location of oishii
-//         const oishii = { lat: 42.343349, lng: -71.066010 };
-//         // The map, centered at oishii
-//         const map = new google.maps.Map(
-//         document.getElementById('map'), { zoom: 15, center: oishii },
-//         );
-//         // The marker, positioned at oishii
-//     const marker = new google.maps.Marker({ position: oishii, map });
-//   }
-// })
-
-/* eslint-disable */
-import {GoogleMap} from '@googlemaps/map-loader';
+import { GoogleMap } from '@googlemaps/map-loader';
 
 /*
  * Set your API key here.
  * For more information on generating API keys,
  * see https://goo.gle/gmp-generate-api-key-video
  */
-const googleMapsAPIKey = "AIzaSyBVizxDTK9ovtbn2UZeZ69xDvTO0IY5pJg";
+const googleMapsAPIKey = 'AIzaSyBVizxDTK9ovtbn2UZeZ69xDvTO0IY5pJg';
 
 /*
  * Options for how the map should initially render.
@@ -32,18 +15,18 @@ const googleMapsAPIKey = "AIzaSyBVizxDTK9ovtbn2UZeZ69xDvTO0IY5pJg";
 const mapOptions = {
   center: {
     lat: 47.649196,
-    lng: -122.350384
+    lng: -122.350384,
   },
-  zoom: 12
-}
+  zoom: 12,
+};
 
 /*
  * Options for loading the Maps JS API.
  */
 const apiOptions = {
   version: 'weekly',
-  libraries: ['places']
-}
+  libraries: ['places'],
+};
 
 /*
  * Set ID of the div where the map will be loaded,
@@ -53,8 +36,8 @@ const mapLoaderOptions = {
   apiKey: googleMapsAPIKey,
   divId: 'google_map',
   append: true, // Appends to divId. Set to false to init in divId.
-  mapOptions: mapOptions,
-  apiOptions: apiOptions
+  mapOptions,
+  apiOptions,
 };
 
 // Instantiate map loader
@@ -63,7 +46,7 @@ const mapLoader = new GoogleMap();
 // Load the map
 mapLoader
   .initMap(mapLoaderOptions)
-  .then(googleMap => {
-    console.log('map: ', googleMap)
-    return googleMap
+  .then((googleMap) => {
+    console.log('map: ', googleMap);
+    return googleMap;
   });
