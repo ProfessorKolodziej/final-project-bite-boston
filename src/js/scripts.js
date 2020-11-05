d5437b29eb5b2f4c4f2b6be41538177d7188e1db
Ashley Kolodziej <kolodz10@bu.edu>
2020-11-05 17:24:22 -0500

    Run fix command, remove unused variables

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,7 @@ import restaurantList from './restaurant-data';
 const googleMapsAPIKey = process.env.MAP_KEY;
 const mapOptions = {
   center: {
-    lat: 42.343349, lng: -71.066010
+    lat: 42.343349, lng: -71.066010,
   },
   zoom: 13,
 };
@@ -33,94 +33,93 @@ const mapLoaderOptions = {
 // Instantiate map loader
 const mapLoader = new GoogleMap();
 
-const makeMarker = function(map, position, icon) {
+const makeMarker = function (map, position, icon) {
   return new MarkerWithLabel({
-    position: position,
+    position,
     draggable: false,
     clickable: true,
-    map: map,
+    map,
     labelContent: '',
-    icon: icon
+    icon,
   });
-}
+};
 
-//home page info-window.
+// home page info-window.
 function mapInfoWindow(restaurant) {
-  return '<div class="info-card">' +
-    '<div class="image-wrapper">' +
-      `<img src=${restaurant.image} class="info-card-image" alt="restaurant-img"/>` +
-    '</div>' +
-    `<h3> ${restaurant.name} </h3>` +
-    `<p> ${restaurant.address} </p>` +
-    `<p> ${restaurant.phone} </p>`+
-    `<a type="button" class="indo-card-details" href=${restaurant.url} target="_blank">Details</a>` +
-  '</div>'
+  return '<div class="info-card">'
+    + '<div class="image-wrapper">'
+      + `<img src=${restaurant.image} class="info-card-image" alt="restaurant-img"/>`
+    + '</div>'
+    + `<h3> ${restaurant.name} </h3>`
+    + `<p> ${restaurant.address} </p>`
+    + `<p> ${restaurant.phone} </p>`
+    + `<a type="button" class="indo-card-details" href=${restaurant.url} target="_blank">Details</a>`
+  + '</div>';
 }
-
 
 mapLoader.initMap(mapLoaderOptions)
   .then((map) => {
-    restaurantList.forEach(function(restaurant) {
-      let marker = makeMarker(map, new google.maps.LatLng(restaurant.location.lat, restaurant.location.lng), restaurant.icon);
-      const placeInfo = mapInfoWindow(restaurant)
+    restaurantList.forEach((restaurant) => {
+      const marker = makeMarker(map, new google.maps.LatLng(restaurant.location.lat, restaurant.location.lng), restaurant.icon);
+      const placeInfo = mapInfoWindow(restaurant);
 
       const info = new google.maps.InfoWindow({
-        content: placeInfo //this should be a string
+        content: placeInfo, // this should be a string
       });
 
-      marker.addListener("click", function (e) {
+      marker.addListener('click', (e) => {
         info.open(map, marker);
       });
-    })
+    });
   });
 
-//restaurant list page
+// restaurant list page
 function mapInfoRestaurantList(restaurant) {
-  return '<div class="info-card">' +
-  `<h3> ${restaurant.name} </h3>` +
-  `<p> ${restaurant.phone} </p>`+
-    '<div class="image-wrapper">' +
-      `<img src=${restaurant.image} class="info-card-image" alt="restaurant-img"/>` +
-    '</div>' +
-    `<p> ${restaurant.address} </p>` +
-    `<p> ${restaurant.introduction} </p>` +
-    `<a type="button" class="indo-card-details" href=${restaurant.url} target="_blank">Details</a>` +
-  '</div>'
+  return '<div class="info-card">'
+  + `<h3> ${restaurant.name} </h3>`
+  + `<p> ${restaurant.phone} </p>`
+    + '<div class="image-wrapper">'
+      + `<img src=${restaurant.image} class="info-card-image" alt="restaurant-img"/>`
+    + '</div>'
+    + `<p> ${restaurant.address} </p>`
+    + `<p> ${restaurant.introduction} </p>`
+    + `<a type="button" class="indo-card-details" href=${restaurant.url} target="_blank">Details</a>`
+  + '</div>';
 }
 
-function render(){
-  restaurantList.forEach(function(restaurant){
-    var ul= document.getElementById("restaurant-list");
-    var li = document.createElement("li");
-    li.innerHTML=mapInfoRestaurantList(restaurant);
+function render() {
+  restaurantList.forEach((restaurant) => {
+    const ul = document.getElementById('restaurant-list');
+    const li = document.createElement('li');
+    li.innerHTML = mapInfoRestaurantList(restaurant);
     ul.appendChild(li);
   });
 }
-document.addEventListener("DOMContentLoaded", render);
+document.addEventListener('DOMContentLoaded', render);
 
-// Restaurant detail page scripts 
+// Restaurant detail page scripts
 
 function restaurantdetailheader(restaurant) {
-  return '<div class="restaurant-detail-name">' +
-    `<h2> ${restaurant.name} </h2>` +
-    '<div class="image-wrapper">' + 
-      `<img src=${restaurant.image} class="restaurant-page-image" alt="restaurant-img"/>` +
-    '</div>' +
-    `<p> ${restaurant.phone} </p>` +
-  '</div>'
+  return '<div class="restaurant-detail-name">'
+    + `<h2> ${restaurant.name} </h2>`
+    + '<div class="image-wrapper">'
+      + `<img src=${restaurant.image} class="restaurant-page-image" alt="restaurant-img"/>`
+    + '</div>'
+    + `<p> ${restaurant.phone} </p>`
+  + '</div>';
 }
 function restaurantdetailbox(restaurant) {
-  return '<div class="restaurant-detail-info">' +
-    `<p> ${restaurant.address} </p>` +
-    `<p> ${restaurant.hours} </p>` +
-    `<p> ${restaurant.price} </p>` +
-    `<p> ${restaurant.cuisines} </p>` +
-    `<p> ${restaurant.diningstyle} </p>` +
-    `<p> ${restaurant.dresscode} </p>` +
-  '</div>'
+  return '<div class="restaurant-detail-info">'
+    + `<p> ${restaurant.address} </p>`
+    + `<p> ${restaurant.hours} </p>`
+    + `<p> ${restaurant.price} </p>`
+    + `<p> ${restaurant.cuisines} </p>`
+    + `<p> ${restaurant.diningstyle} </p>`
+    + `<p> ${restaurant.dresscode} </p>`
+  + '</div>';
 }
 function restaurantdetailintro(restaurant) {
-  return '<div class="restaurant-detail-intro">' +
-    `<p> ${restaurant.bio} </p>` +
-  '</div>'
+  return '<div class="restaurant-detail-intro">'
+    + `<p> ${restaurant.bio} </p>`
+  + '</div>';
 }
