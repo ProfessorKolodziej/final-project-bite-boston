110596cb632526c475f746917870ac76ed88dd3e
Grace Wise <gjsw@bu.edu>
2020-11-18 12:09:57 -0500

    fixed small type errors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -158,7 +158,7 @@ document.addEventListener('click', (event) => {
 // this hides the filter button
 document.addEventListener('click', (event) => {
   if (event.target.className === 'info-card-details-button') {
-    const filter = document.getElementsByClassName('dropbtn');
+    const filter = document.getElementsByClassName("dropbtn");
     const bttn = filter[0];
     if (bttn.style.display === 'none') {
       bttn.style.display = 'block';
