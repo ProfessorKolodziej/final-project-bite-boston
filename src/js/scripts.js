dd34d7df5d29c8e348784bfcebc03616c62fbb5e
Grace Wise <gjsw@bu.edu>
2020-12-04 19:04:56 -0500

    trying to get read more button to work

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -275,7 +275,7 @@ function restaurantDetail(restaurant) {
   + '</div>'
   + '<div class="restaurant-detail-intro">'
   + `<h3>Introduction</h3>`
-  + `<p> id="shrinkMe" class="shrinkable" ${restaurant.introduction} </p>`
+  + `<p id="shrinkMe" class="shrinkable"> ${restaurant.introduction} </p>`
   + '</div>'
   + '</section>';
   }
@@ -322,6 +322,33 @@ function restaurantDetail(restaurant) {
           }
         }
       });
+// //trying out read more buttton
+//       function showMore(id){
+//         document.getElementById(id+'Overflow').className='';
+//         document.getElementById(id+'MoreLink').className='hidden';
+//         document.getElementById(id+'LessLink').className='';
+//     }
+    
+//     function showLess(id){
+//         document.getElementById(id+'Overflow').className='hidden';
+//         document.getElementById(id+'MoreLink').className='';
+//         document.getElementById(id+'LessLink').className='hidden';
+//     }
+    
+//     var len = 100;
+//     var shrinkables = document.getElementsByClassName('shrinkable');
+//     if (shrinkables.length > 0) {
+//         for (var i = 0; i < shrinkables.length; i++){
+//             var fullText = shrinkables[i].innerHTML;
+//             if(fullText.length > len){
+//                 var trunc = fullText.substring(0, len).replace(/\w+$/, '');
+//                 var remainder = "";
+//                 var id = shrinkables[i].id;
+//                 remainder = fullText.substring(len, fullText.length);
+//                 shrinkables[i].innerHTML = '<span>' + trunc + '<span class="hidden" id="' + id + 'Overflow">'+ remainder +'</span></span>&nbsp;<a id="' + id + 'MoreLink" href="#!" onclick="showMore(\''+ id + '\');">More</a><a class="hidden" href="#!" id="' + id + 'LessLink" onclick="showLess(\''+ id + '\');">Less</a>';
+//             }
+//         }
+//     }
 
 
 //filter
