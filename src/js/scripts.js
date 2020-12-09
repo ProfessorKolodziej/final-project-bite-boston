b06fd0d4c6dc93353743336c86cdcaa52a0e8972
Ran Chen <rchen716@bu.edu>
2020-12-10 00:31:03 +0800

    list page responsive design

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -330,3 +330,13 @@ for (const checkbox of checkboxes) {
   }
 }*/
 
+// filter button
+let dropdownBtn = document.querySelector('.menu-btn');
+let menuContent = document.querySelector('.menu-content');
+dropdownBtn.addEventListener('click', () => {
+    if (menuContent.style.display === "") {
+        menuContent.style.display = "block";
+    } else {
+        menuContent.style.display = "";
+    }
+});
